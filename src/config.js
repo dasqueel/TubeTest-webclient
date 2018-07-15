9a1db6f97246c77c869dfe6fbbf0c3ebd3466a5d
Neil Barduson <barduson.neil@gmail.com>
2018-07-15 16:43:53 -0500

    switch to the remote api url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-// const apiUrl = process.env['REACT_APP_API_URL_REMOTE'];
-const apiUrl = process.env['REACT_APP_API_URL_LOCAL'];
+const apiUrl = process.env['REACT_APP_API_URL_REMOTE'];
+// const apiUrl = process.env['REACT_APP_API_URL_LOCAL'];
 
 module.exports = {
   apiUrl
-}
+};
